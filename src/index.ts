65d21d5fdeaa6e7c60146e87c7e2327d41dff4ea
Trịnh Khánh <realkripper@gmail.com>
2023-11-02 17:44:10 +0700

    compact index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import router from './routes';
 import dotenv from 'dotenv';
+import connectDB from './utils/connectDB';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose
-    .connect(process.env.MONGODB_URL!)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+connectDB();
+
 app.use(express.json());
 app.use('/', router)
 
